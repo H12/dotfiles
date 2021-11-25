b8a01d608a85114ff40f90979388b5bdbb3d91b2
Henry Firth <henrynf12@gmail.com>
2021-11-25 15:28:20 -0600

    fixup! Set up gruvbox theme

--- neovim/plugins/gruvbox.lua
+++ neovim/plugins/gruvbox.lua
@@ -1,4 +1,4 @@
 vim.cmd("let g:gruvbox_material_transparent_background = 1")
-vim.cmd("let g:gruvbox_enable_bold = 1")
+vim.cmd("let g:gruvbox_material_enable_bold = 1")
 
 vim.cmd("colorscheme gruvbox-material")
