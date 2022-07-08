379e1873c0462a40a2cf90a257b0039dc15bf511
Henry Firth <henrynf12@gmail.com>
2022-07-08 10:27:39 -0500

    Harden hammerspoon window management

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,34 +1,49 @@
 hs.window.animationDuration = 0 -- disable animations
 
-local defaultPadding = 8
 local defaultMash  = { 'ctrl', 'alt', 'cmd' }
 
-function move(x, y, w, h, p)
-	p = p or defaultPadding
+function move(x, y, w, h, c, r, i, j, p)
+	-- NOTE: In order to have sensible defaults, it is assumed that the
+	-- maximum number of rows or columns is two. To create window
+	-- arrangements with more than two rows or columns, values must be
+	-- provided for c, r, i, and j.
 
-	local win = hs.window.focusedWindow()
-	local f = win:frame()
-	local screen = win:screen()
-	local max = screen:frame()
-	local aspectRatio = max.w / max.h
+	-- Default col/row count is 1 if w/h is 1, 2 if w/h is less than 1
+	local col_count = c or (w == 1 and 1 or 2)
+	local row_count = r or (h == 1 and 1 or 2)
 
-	local xOffset = (x == 0) and 0 or p / 2
-	local yOffset = (y == 0) and 0 or p / 2
-	local wOffset = (w == 1) and 2 or 1.5
-	local hOffset = (h == 1) and 2 or 1.5
+	-- Default col/row index is 0 if x/y is 0, 1 if x/y is greater than 0
+	local col_index = i or (x == 0 and 0 or 1)
+	local row_index = j or (y == 0 and 0 or 1)
 
-	f.x = max.x + (max.w * x) + (p - xOffset)
-	f.y = max.y + (max.h * y) + (p - yOffset)
-	f.w = max.w * w - (p * wOffset)
-	f.h = max.h * h - (p * hOffset)
+	-- Get window & screen objects
+	local window = hs.window.focusedWindow()
+	local screen = window:screen()
+	local windowFrame = window:frame()
+	local screenFrame = screen:frame()
 
-	-- If calling `center`...
-	if (p > defaultPadding) then
-		f.w = f.w - (p * aspectRatio) * 2
-		f.x = f.x + (p * aspectRatio)
-	end
+	-- Set padding default to a value proportional to diagonal screen size
+	local screenSize = math.sqrt(screenFrame.w ^ 2 + screenFrame.h ^ 2)
+	local padding = p or screenSize / 200
 
-	win:setFrame(f)
+	-- Set raw window positions & sizes without padding
+	local xRaw = screenFrame.x + (screenFrame.w * x)
+	local yRaw = screenFrame.y + (screenFrame.h * y)
+	local wRaw = screenFrame.w * w
+	local hRaw = screenFrame.h * h
+
+	-- Set position & size offsets from padding, row/col count, & index
+	local xOffset = padding * (col_count - col_index) / col_count
+	local yOffset = padding * (row_count - row_index) / row_count
+	local wOffset = padding * (col_count + 1) / col_count
+	local hOffset = padding * (row_count + 1) / row_count
+
+	-- Apply position and width values to the window
+	windowFrame.x = xRaw + xOffset
+	windowFrame.y = yRaw + yOffset
+	windowFrame.w = wRaw - wOffset
+	windowFrame.h = hRaw - hOffset
+	window:setFrame(windowFrame)
 end
 
 function bindHotkey(key, func, mash)
@@ -36,7 +51,7 @@ function bindHotkey(key, func, mash)
 	hs.hotkey.bind(mash, key, func)
 end
 
-function center()        move(0.0, 0.0, 1.0, 1.0, 84) end
+function center()        move(0.125, 0.075, 0.75, 0.85) end
 function fill()          move(0.0, 0.0, 1.0, 1.0) end
 function top50()         move(0.0, 0.0, 1.0, 0.5) end
 function left60()        move(0.0, 0.0, 0.58, 1.0) end
@@ -54,6 +69,10 @@ function topLeft40()     move(0.0, 0.0, 0.42, 0.5) end
 function topRight40()    move(0.58, 0.0, 0.42, 0.5) end
 function bottomLeft40()  move(0.0, 0.5, 0.42, 0.5) end
 function bottomRight40() move(0.58, 0.5, 0.42, 0.5) end
+
+function firstThird() move(0.0, 0.0, 1/3, 1.0, 3, 1, 0, 0) end
+function secndThird() move(1/3, 0.0, 1/3, 1.0, 3, 1, 1, 0) end
+function thirdThird() move(2/3, 0.0, 1/3, 1.0, 3, 1, 2, 0) end
 
 bindHotkey("C", center)
 bindHotkey("F", fill)
@@ -73,3 +92,7 @@ bindHotkey("5", topLeft40)
 bindHotkey("6", topRight40)
 bindHotkey("7", bottomRight40)
 bindHotkey("8", bottomLeft40)
+
+bindHotkey(",", firstThird)
+bindHotkey(".", secndThird)
+bindHotkey("/", thirdThird)
