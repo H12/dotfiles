9358bb8d0562427c042b089efab8a267f60619f6
Henry Firth <henrynf12@gmail.com>
2022-08-19 07:46:47 -0500

    Switch to using fzf-native for telescope

--- neovim/plugins.lua
+++ neovim/plugins.lua
@@ -36,6 +36,8 @@ return require('packer').startup(function()
 		requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
 	}
 
+	use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+
 	-- Statusline
 	use 'itchyny/vim-gitbranch'
 	use {
--- neovim/plugins/telescope.lua
+++ neovim/plugins/telescope.lua
@@ -11,6 +11,16 @@ vim.api.nvim_set_keymap('n',
 	':Telescope live_grep<cr>',
 	{ noremap = true, silent = true })
 
+vim.api.nvim_set_keymap('n',
+	'<C-g>',
+	':Telescope grep_string<cr>',
+	{ noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('v',
+	'<C-g>',
+	'"zy<cmd>exec \'Telescope grep_string default_text=\' . escape(@z, \' \')<cr>',
+	{ noremap = true, silent = true })
+
 require('telescope').setup({
 	defaults = {
 		layout_strategy = 'flex',
@@ -22,10 +32,18 @@ require('telescope').setup({
 				},
 			},
 		},
-		vimgrep_arguments = {
-			'ag',
-			'--hidden',
-			'--vimgrep',
-		},
 	},
+	extensions = {
+		fzf = {
+			fuzzy = true,                    -- false will only do exact matching
+			override_generic_sorter = true,  -- override the generic sorter
+			override_file_sorter = true,     -- override the file sorter
+			case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+			-- the default case_mode is "smart_case"
+		}
+	}
 })
+
+-- To get fzf loaded and working with telescope, you need to call
+-- load_extension, somewhere after setup function:
+require('telescope').load_extension('fzf')
