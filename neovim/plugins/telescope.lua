bc0dc518567c54ad33da7280aba725203e188138
Henry Firth <henrynf12@gmail.com>
2023-03-26 11:56:51 -0500

    Change file sorting in Telescope file browser

--- neovim/plugins/telescope.lua
+++ neovim/plugins/telescope.lua
@@ -28,7 +28,7 @@ vim.api.nvim_set_keymap('v',
 
 vim.api.nvim_set_keymap('n',
 	'<C-b>',
-	':Telescope file_browser path="%:p:h"<cr>',
+	':Telescope file_browser grouped=true<cr>',
 	{ noremap = true, silent = true })
 
 require('telescope').setup({
