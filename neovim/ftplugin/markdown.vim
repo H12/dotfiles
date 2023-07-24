83c38506d403326f20513d3774775ebffdc60c74
Henry Firth <henrynf12@gmail.com>
2023-07-24 07:08:48 -0500

    Wipe away neovim config

--- neovim/ftplugin/markdown.vim
+++
@@ -1,7 +0,0 @@
-set tw=80
-set spell
-set wrap
-set linebreak
-set tabstop=2
-set softtabstop=2
-set shiftwidth=2
--- neovim/init.lua
+++ neovim/init.lua
@@ -1,26 +1,21 @@
-require('plugins')
-require('lsp')
-
-vim.cmd([[ syntax on ]])
-
 -- Turn on termguicolors
 vim.o.termguicolors = true
 
 -- Disable line wrapping
-vim.o.nowrap = false
+vim.o.wrap = false
 
 -- Turn on relative line numbers
 vim.o.number = true
 vim.o.relativenumber = true
 
 -- Turn of default greeter page
-vim.o.shortmess = 'i'
+-- vim.o.shortmess = 'i'
 
 -- Enable SignColumn globally
-vim.o.signcolumn = 'yes'
+-- vim.o.signcolumn = 'yes'
 
 -- Disable SignColumn on the greeter page
-vim.opt_local.signcolumn = 'no'
+-- vim.opt_local.signcolumn = 'no'
 
 -- Highlight search incrementally but not permanently
 vim.opt.incsearch = true
@@ -42,11 +37,11 @@ vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz', { noremap = true })
 
 -- Use Floaterm for Terminal one-offs
-vim.api.nvim_create_user_command('T',
-	'T FloatermNew --autoclose=0 --height=0.9 --width=0.9 --title=Command <args>',
-	{}
-)
-
-vim.api.nvim_create_autocmd("VimEnter", {
-	command = 'FloatermNew --silent --height=0.7 --width=0.6 --title=Terminal --name=default'
-})
+-- vim.api.nvim_create_user_command('T',
+-- 	'T FloatermNew --autoclose=0 --height=0.9 --width=0.9 --title=Command <args>',
+-- 	{}
+-- )
+-- 
+-- vim.api.nvim_create_autocmd("VimEnter", {
+-- 	command = 'FloatermNew --silent --height=0.7 --width=0.6 --title=Terminal --name=default'
+-- })
