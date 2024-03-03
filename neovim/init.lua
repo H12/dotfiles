007624d37052580bf5267dcf7f8be09959a7c340
Henry Firth <henrynf12@gmail.com>
2024-03-03 16:49:07 -0600

    Turn on rust lsp

--- neovim/init.lua
+++ neovim/init.lua
@@ -518,7 +518,7 @@ local servers = {
   },
   gopls = {},
   -- pyright = {},
-  -- rust_analyzer = {},
+  rust_analyzer = {},
   tsserver = {},
   html = { filetypes = { 'html', 'twig', 'hbs' } },
 
